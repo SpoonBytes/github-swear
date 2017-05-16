4b6479732b48d7918f6edd9a67e81121f19fbba3
Spencer Berenson <spencer@berenson.me>
2017-05-16 19:31:01 -0400

    More changes.

--- app.js
+++ app.js
@@ -14,67 +14,63 @@ github.authenticate({
 
 const languages = {};
 
-fs.readFile('swears.txt', (err, data) => {
-	if(err) throw err;
+const swearsList = fs.readFileSync('swears.txt').toString().split("\n");
+const swears = [];
+let swear;
+let swearIndex = 0;
+for(swear of swearsList) {
+	swears.push({
+		word: swear,
+		pages: null,
+		pageNumber: 1,
+		files: []
+	});
+}
 
-	const swearsText = data.toString().split("\n");
-	const swears = [];
-	let swear;
-	let swearIndex = 0;
-	for(swear of swearsText) {
-		swears.push({
-			word: swear,
-			pages: null,
-			pageNumber: 1,
-			files: []
-		});
+console.log(`Beginning search for swear word "${swears[swearIndex].word}" on page 1."`);
+
+// Search API requests are limited to 30 per minute.
+// A request is performed every two seconds (totaling 30 requests per minute) to meet the requirements.
+const rateLimiter = setInterval(() => {
+	if(swearIndex >= swears.length) {
+		clearInterval(rateLimiter);
+		return;
 	}
 
-	console.log(`Beginning search for swear word "${swears[swearIndex]}."`);
-
-	// Search API requests are limited to 30 per minute.
-	// A request is performed every two seconds (totaling 30 requests per minute) to meet the requirements.
-	const rateLimiter = setInterval(() => {
-		if(swearIndex >= swears.length) {
-			clearInterval(rateLimiter);
-			return;
+	swear = swears[swearIndex];
+	github.search.code({
+		q: swear.word + " in:file",
+		page: swear.pageNumber,
+		per_page: 100
+	}).then(result => {
+		if(!swears[swearIndex].pages) {
+			const pages = Number(result.meta.link.split(",")[1].split("&page=")[1].split("&")[0]); // Hacky, but works.
+			swears[swearIndex].pages = pages;
 		}
 
-		swear = swears[swearIndex];
-		github.search.code({
-			q: swear.word + " in:file",
-			page: swear.pageNumber,
-			per_page: 100
-		}).then(result => {
-			if(!swears[swearIndex].pages) {
-				const pages = result.meta.link.split(",")[1].split("&page=")[1].split("&")[0]; // Hacky, but works.
-				swears[swearIndex].pages = pages;
+		const code = result.data.items;
+		for(file of code) {
+			const extension = path.extname(file.path);
+			if(!languages[extension]) {
+				languages[extension] = {};
+			}
+			if(!languages[extension][swear.word]) {
+				languages[extension][swear.word] = 0;
 			}
 
-			const code = result.data.items;
-			for(file of code) {
-				const extension = path.extname(file.path);
-				if(!languages[extension]) {
-					languages[extension] = {};
-				}
-				if(!languages[extension][swear.word]) {
-					languages[extension][swear.word] = 0;
-				}
+			languages[extension][swear.word] += 1;
+		}
 
-				languages[extension][swear.word] += 1;
-			}
-
-			if(swear.pageNumber === swear.pages) {
-				console.log(`Ending search for swear word "${swears[swearIndex].word}."`);
-				swearIndex += 1;
-				console.log(`Starting next search for swear word "${swears[swearIndex].word}."`);
-			} else {
-				swears[swearIndex].pageNumber += 1;
-				console.log(`Continuing search for swear word "${swears[swearIndex].word}" on page ${swears[swearIndex].pageNumber}.`);
-			}
-		}).catch(err => console.error);
-	}, 2000)
-});
+		if(swears[swearIndex].pageNumber === swears[swearIndex].pages) {
+			console.log(`Ending search for swear word "${swears[swearIndex].word}."`);
+			swearIndex += 1;
+			console.log(`Starting next search for swear word "${swears[swearIndex].word} on page 1."`);
+		} else {
+			swears[swearIndex].pageNumber += 1;
+			console.log(`Continuing search for swear word "${swears[swearIndex].word}" on page ${swears[swearIndex].pageNumber}.`);
+		}
+	}).catch(err => console.error);
+}, 2000);
 
 death((signal, err) => {
 	console.log("Searching has been stopped. Please wait while results are stored...");

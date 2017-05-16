987f6ef813c73022751d0f9723e38bf35e86f238
Spencer Berenson <spencer@berenson.me>
2017-05-16 15:51:24 -0400

    Updated to poll for recurrence in contents of file rather than number of files returned.

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ fs.readFile('swears.txt', (err, data) => {
 		});
 	}
 
+	// Search API requests are limited to 30 per minute.
+	// A request is performed every two seconds (totalling 30 requests per minute) to meet the requirements.
 	const rateLimiter = setInterval(() => {
 		if(swearIndex > swears.length) {
 			clearInterval(rateLimiter);
@@ -49,7 +51,18 @@ fs.readFile('swears.txt', (err, data) => {
 				}
 
 				const code = result.data.items;
-				for(file of code) {
+				let codeIndex = 0;
+
+				// Regular API requests are limited to 5,000 per hour, so around 83 per minute.
+				// A request is performed every second (totalling 60 requests per minute) to meet the requirements.
+				// Although this could be performed faster, it is kept at a lower rate to account for potential variablity in timing.
+				const contentRateLimiter = setInterval(() => {
+					if(codeIndex > code.length) {
+						clearInterval(contentRateLimiter);
+						return;
+					}
+
+					const file = code[codeIndex];
 					const extension = path.extname(file.path);
 					if(!languages[extension]) {
 						languages[extension] = {};
@@ -57,15 +70,25 @@ fs.readFile('swears.txt', (err, data) => {
 					if(!languages[extension][swear.word]) {
 						languages[extension][swear.word] = 0;
 					}
-					languages[extension][swear.word] += 1;
-				}
-			}).catch((err) => console.error);
+
+					github.repos.getContent({
+						owner: file.repository.owner.login,
+						repo: file.repository.name,
+						path: file.path
+					}).then(result => {
+						const fileContent = new Buffer(result.data.content, "base64").toString("utf8");
+						languages[extension][swear.word] += (fileContent.match(new RegExp(swear.word, "g")) || []).length;
+						codeIndex += 1;
+					}).catch(err => console.error);
+				}, 1000);
+
+			}).catch(err => console.error);
 		}
 	}, 2000)
 });
 
 death((signal, err) => {
-	jsonFile.writeFile("./results.json", languages, (err) => {
+	jsonFile.writeFile("./results.json", languages, err => {
 		if(err) console.error(err);
 		process.exit();
 	});

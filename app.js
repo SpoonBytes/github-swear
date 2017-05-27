778de5a2272c9b8bc5d768a64d79b77de598d78b
Spencer Berenson <spencer@berenson.me>
2017-05-27 11:14:06 -0400

    Added memory restrictions for Digital Ocean server.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const os = require("os");
 const fs = require("fs");
 const path = require("path");
 const death = require("death")({ uncaughtException: true });
@@ -27,6 +28,10 @@ for(swear of swears) {
 	}
 }
 
+function isMemoryAvailable() {
+	return os.freemem() >= 150000000;
+}
+
 console.log(`Searching will begin now.`);
 
 // GitHub Search API requests are limited to 30 per minute.
@@ -34,80 +39,84 @@ console.log(`Searching will begin now.`);
 // This makes a total of 30 API requests per minute.
 
 const baseTimer = setInterval(() => {
-	if(baseQueue.length) {
-		const query = baseQueue[0];
-		baseQueue.shift();
+	if(isMemoryAvailable()) {
+		if(baseQueue.length) {
+			const query = baseQueue[0];
+			baseQueue.shift();
 
-		console.log(`Beginning search for ${query.language} files containing "${query.word}" on page 1.`);
+			console.log(`Beginning search for ${query.language} files containing "${query.word}" on page 1.`);
 
-		github.search.code({
-			q: `${query.word} in:file language:${query.language}`,
-			per_page: 100,
-			page: 1
-		}).then(result => {
-			if(result.meta.link && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0])) {
-				const nextPage = Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]); // Hacky, but works.
-				pageQueue.push({
-					word: query.word,
-					language: query.language,
-					nextPage: nextPage
-				});
-			}
+			github.search.code({
+				q: `${query.word} in:file language:${query.language}`,
+				per_page: 100,
+				page: 1
+			}).then(result => {
+				if(result.meta.link && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0])) {
+					const nextPage = Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]); // Hacky, but works.
+					pageQueue.push({
+						word: query.word,
+						language: query.language,
+						nextPage: nextPage
+					});
+				}
 
-			for(file of result.data.items) {
-				contentQueue.push({
-					word: query.word,
-					language: query.language,
-					repo: file.repository.name,
-					owner: file.repository.owner.login,
-					path: file.path
-				});
-			}
-		}).catch(err => {
-			if(err.message.includes("abuse detection mechanism")) {
-				console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
-				baseQueue.unshift(query);
-			} else console.log(err);
-		});
+				for(file of result.data.items) {
+					contentQueue.push({
+						word: query.word,
+						language: query.language,
+						repo: file.repository.name,
+						owner: file.repository.owner.login,
+						path: file.path
+					});
+				}
+			}).catch(err => {
+				if(err.message.includes("abuse detection mechanism")) {
+					console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
+					baseQueue.unshift(query);
+				} else console.log(err);
+			});
+		}
 	}
 }, 4000);
 
 const pageTimer = setInterval(() => {
-	if(pageQueue.length) {
-		const query = pageQueue[0];
-		pageQueue.shift();
+	if(isMemoryAvailable()) {
+		if(pageQueue.length) {
+			const query = pageQueue[0];
+			pageQueue.shift();
 
-		console.log(`Continuing search for ${query.language} files containing "${query.word}" on page ${query.nextPage}.`);
+			console.log(`Continuing search for ${query.language} files containing "${query.word}" on page ${query.nextPage}.`);
 
-		github.search.code({
-			q: `${query.word} in:file language:${query.language}`,
-			per_page: 100,
-			page: query.nextPage
-		}).then(result => {
-			if(result.meta.link && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0])) {
-				const nextPage = Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]); // Hacky, but works.
-				pageQueue.push({
-					word: query.word,
-					language: query.language,
-					nextPage: nextPage
-				});
-			}
+			github.search.code({
+				q: `${query.word} in:file language:${query.language}`,
+				per_page: 100,
+				page: query.nextPage
+			}).then(result => {
+				if(result.meta.link && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0])) {
+					const nextPage = Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]); // Hacky, but works.
+					pageQueue.push({
+						word: query.word,
+						language: query.language,
+						nextPage: nextPage
+					});
+				}
 
-			for(file of result.data.items) {
-				contentQueue.push({
-					word: query.word,
-					language: query.language,
-					repo: file.repository.name,
-					owner: file.repository.owner.login,
-					path: file.path
-				});
-			}
-		}).catch(err => {
-			if(err.message.includes("abuse detection mechanism")) {
-				console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
-				pageQueue.unshift(query);
-			} else console.log(err);
-		});
+				for(file of result.data.items) {
+					contentQueue.push({
+						word: query.word,
+						language: query.language,
+						repo: file.repository.name,
+						owner: file.repository.owner.login,
+						path: file.path
+					});
+				}
+			}).catch(err => {
+				if(err.message.includes("abuse detection mechanism")) {
+					console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
+					pageQueue.unshift(query);
+				} else console.log(err);
+			});
+		}
 	}
 }, 4000);
 
@@ -116,33 +125,35 @@ const pageTimer = setInterval(() => {
 // This makes a total of 60 API requests per minute.
 
 const contentTimer = setInterval(() => {
-	if(contentQueue.length) {
-		const query = contentQueue[0];
-		contentQueue.shift();
+	if(isMemoryAvailable()) {
+		if(contentQueue.length) {
+			const query = contentQueue[0];
+			contentQueue.shift();
 
-		console.log(`Searching ${query.language} file content for instances of "${query.word}."`);
+			console.log(`Searching ${query.language} file content for instances of "${query.word}."`);
 
-		github.repos.getContent({
-			owner: query.owner,
-			repo: query.repo,
-			path: query.path
-		}).then(result => {
-			const content = new Buffer(result.data.content, "base64").toString("utf8");
+			github.repos.getContent({
+				owner: query.owner,
+				repo: query.repo,
+				path: query.path
+			}).then(result => {
+				const content = new Buffer(result.data.content, "base64").toString("utf8");
 
-			if(!results[query.language]) {
-				results[query.language] = {};
-			}
-			if(!results[query.language][query.word]) {
-				results[query.language][query.word] = 0;
-			}
-			results[query.language][query.word] += (content.match(new RegExp(query.word, "g")) || []).length;
-		}).catch(err => {
-			if(err.message.includes("abuse detection mechanism")) {
-				console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
-				contentQueue.unshift(query);
-			}
-			else console.log(err);
-		});
+				if(!results[query.language]) {
+					results[query.language] = {};
+				}
+				if(!results[query.language][query.word]) {
+					results[query.language][query.word] = 0;
+				}
+				results[query.language][query.word] += (content.match(new RegExp(query.word, "g")) || []).length;
+			}).catch(err => {
+				if(err.message.includes("abuse detection mechanism")) {
+					console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
+					contentQueue.unshift(query);
+				}
+				else console.log(err);
+			});
+		}
 	}
 }, 1000);
 

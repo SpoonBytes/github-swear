3eba06394dd563841e51e115c56474ea2cc00937
Andi Andreas <nexuister@gmail.com>
2017-06-03 12:30:28 -0400

    Modified baseQueue to support searching multiple swears in the same query

--- app.js
+++ app.js
@@ -19,17 +19,14 @@ const baseQueue = [];
 const pageQueue = [];
 const contentQueue = [];
 const results = {};
-for(swear of swears) {
-	for(language of languages) {
-		baseQueue.push({
-			word: swear.replace("\r", ""),
-			language: language
-		});
+for (language of languages) {
+	for (i = 0; i < swears.length; i = i + 3) {
+		baseQueue.push(`${swears[i]} OR ${swears[i + 1]} OR ${swears[i + 2]} in:file language:"${language}"`);
 	}
 }
 
 function memoryIsAvailable() {
-	return os.freemem() >= 150000000;
+	return os.freemem() <= os.totalmem() * .80;
 }
 
 console.log(`Searching will begin now.`);
@@ -38,64 +35,65 @@ console.log(`Searching will begin now.`);
 // Therefore, the baseTimer is run every 4 seconds and pageTimer is run every 4 seconds.
 // This makes a total of 30 API requests per minute.
 
-const baseTimer = setInterval(() => {
-	if(memoryIsAvailable()) {
-		if(baseQueue.length) {
-			const query = baseQueue[0];
-			baseQueue.shift();
+// const baseTimer = setInterval(() => {
+// 	if(memoryIsAvailable()) {
+// 		if(baseQueue.length) {
+// 			const query = baseQueue[0];
+// 			baseQueue.shift();
 
-			console.log(`Beginning search for ${query.language} files containing "${query.word}" on page 1.`);
+// 			console.log(`Beginning search for ${query.language} files containing "${query.phrase}" on page 1.`);
 
-			github.search.code({
-				q: `${query.word} in:file language:${query.language}`,
-				per_page: 100,
-				page: 1
-			}).then(result => {
-				if(result.meta.link && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0])) {
-					const nextPage = Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]); // Hacky, but works.
-					pageQueue.push({
-						word: query.word,
-						language: query.language,
-						nextPage: nextPage
-					});
-				}
+// 			github.search.code({
+// 				q: `${query.phrase} in:file language:${query.language}`,
+// 				per_page: 100,
+// 				page: 1
+// 			}).then(result => {
+// 				if(result.meta.link && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0])) {
+// 					const nextPage = Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]); // Hacky, but works.
+// 					pageQueue.push({
+// 						phrase: query.phrase,
+// 						language: query.language,
+// 						nextPage: nextPage
+// 					});
+// 				}
 
-				for(file of result.data.items) {
-					contentQueue.push({
-						word: query.word,
-						language: query.language,
-						repo: file.repository.name,
-						owner: file.repository.owner.login,
-						path: file.path
-					});
-				}
-			}).catch(err => {
-				if(err.message.includes("abuse detection mechanism")) {
-					console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
-					baseQueue.unshift(query);
-				} else console.log(err);
-			});
-		}
-	}
-}, 4000);
+// 				for(file of result.data.items) {
+// 					contentQueue.push({
+// 						phrase: query.phrase,
+// 						language: query.language,
+// 						repo: file.repository.name,
+// 						owner: file.repository.owner.login,
+// 						path: file.path
+// 					});
+// 				}
+// 			}).catch(err => {
+// 				if(err.message.includes("abuse detection mechanism")) {
+// 					console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
+// 					baseQueue.unshift(query);
+// 				} else console.log(err);
+// 			});
+// 		}
+// 	}
+// }, 4000);
 
+const pageLimit = 1;
 const pageTimer = setInterval(() => {
 	if(memoryIsAvailable()) {
 		if(pageQueue.length) {
 			const query = pageQueue[0];
 			pageQueue.shift();
 
-			console.log(`Continuing search for ${query.language} files containing "${query.word}" on page ${query.nextPage}.`);
+			console.log(`Continuing search for ${query.language} files containing "${query.phrase}" on page ${query.nextPage}.`);
 
 			github.search.code({
-				q: `${query.word} in:file language:${query.language}`,
+				q: `${query.phrase} in:file language:${query.language}`,
 				per_page: 100,
 				page: query.nextPage
 			}).then(result => {
-				if(result.meta.link && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0])) {
+				if(result.meta.link && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]) && Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]) <= pageLimit) {
 					const nextPage = Number(result.meta.link.split(";")[0].split("&page=")[1].split("&")[0]); // Hacky, but works.
 					pageQueue.push({
-						word: query.word,
+						phrase: query.phrase,
 						language: query.language,
 						nextPage: nextPage
 					});
@@ -103,7 +101,7 @@ const pageTimer = setInterval(() => {
 
 				for(file of result.data.items) {
 					contentQueue.push({
-						word: query.word,
+						phrase: query.phrase,
 						language: query.language,
 						repo: file.repository.name,
 						owner: file.repository.owner.login,
@@ -130,7 +128,7 @@ const contentTimer = setInterval(() => {
 			const query = contentQueue[0];
 			contentQueue.shift();
 
-			console.log(`Searching ${query.language} file content for instances of "${query.word}."`);
+			console.log(`Searching ${query.language} file content for instances of "${query.phrase}."`);
 
 			github.repos.getContent({
 				owner: query.owner,
@@ -142,10 +140,13 @@ const contentTimer = setInterval(() => {
 				if(!results[query.language]) {
 					results[query.language] = {};
 				}
-				if(!results[query.language][query.word]) {
-					results[query.language][query.word] = 0;
+				if(!results[query.language][query.phrase]) {
+					results[query.language][query.phrase] = 0;
 				}
-				results[query.language][query.word] += (content.match(new RegExp(query.word, "g")) || []).length;
+				results[query.language][query.phrase] += (content.match(new RegExp(query.phrase, "g")) || []).length;
+				jsonFile.writeFile("./results.json", JSON.stringify(results, null, 4), (err) => {
+					if (err) console.log(`Write failed: ${err}`)
+				});
 			}).catch(err => {
 				if(err.message.includes("abuse detection mechanism")) {
 					console.log("GitHub API rate limit was exceeded. Requeuing query to search again later.")
@@ -156,11 +157,3 @@ const contentTimer = setInterval(() => {
 		}
 	}
 }, 1000);
-
-death((signal, err) => {
-	console.log("Searching has been stopped. Please wait while results are stored...");
-	jsonFile.writeFile("./results.json", JSON.stringify(results, null, 4), err => {
-		if(err) console.error(err);
-		process.exit();
-	})
-});
---
+++ garbage.md
@@ -0,0 +1,10 @@
+- Language
+	- File: Swear Count
+
+
+- JavaScript
+	- garbage.js: 12
+	- bananas.js: 0
+	- fail.js: 124
+
+<swear> or <swear> or <swear> language:JavaScript
